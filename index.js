1c9b8be8bf5f50f9b8d738e64bb6a3260c0a8dda
erick <mazae2000@estudiantec.cr>
2019-11-11 19:54:01 -0600

    primera instruccion del api

--- index.js
+++ index.js
@@ -5,31 +5,44 @@ const port = 4269
 app.use(express.json())// parsea todos los json que entran 
 
 app.get("/prueba",(req,rest)=>{
-rest.send("hola")
+	rest.send("hola")
 })
 
 app.listen(port,() => {
-console.log("App running")
+	console.log("App running")
 })
+app.post("/graphs",(req,res)=>{
+	try{
+		//crear el grafo
+		const grafo= "a"// hay que cambiar por el grafo
+		const newjson = { "id":grafo} //cambiar por el id del grafo
+	}
+	catch(e){
+		rest.set("status",500) 
+		res.status(500).send()
 
+	}
+	
+
+})
 app.get("/prueba/:par1",(req,res)=>{
-const parametro = req.params.par1
-res.send(parametro)
+	const parametro = req.params.par1
+	res.send(parametro)
 })
 
 app.get("/pruebaQuery",(req,rest)=> {
-rest.send(req.query.q1)
+	rest.send(req.query.q1)
 })
 
 app.post("/pruebajason",(req,rest) =>{
-const nomnbre = req.body.entity.nombre
-const edad = req.body.entity.edad
-console.log(nombre)
-const newjson = { "edad":133} // crear un json
-rest.send(newjson)
+	const nomnbre = req.body.entity.nombre
+	const edad = req.body.entity.edad
+	console.log(nombre)
+	const newjson = { "edad":133} // crear un json
+	rest.send(newjson)
 })
 
 app.put("*",(req,res)=>{
-rest.set("status",404) //agregar al header
-res.status(500).send()
+	rest.set("status",404) //agregar al header
+	res.status(500).send()
 })
